ab2a70edc086705a8ba59c9dca6d44e2fc0c4e85
Shekhar Patnaik <patnaikshekhar@gmail.com>
2015-11-06 16:53:56 +0000

    Added command line argument for port

--- bin/logpiper.ts
+++ bin/logpiper.ts
@@ -1,2 +1,12 @@
 import logpiper = require('../src/logpiper')
-logpiper.logpiper(8080, (server) => {});
+import commandLineArgs = require('command-line-args');
+
+let cli = commandLineArgs([
+	{ name: 'port', alias: 'p', type: Number },
+]);
+
+let options = cli.parse()
+
+let port = options.port || 8080;
+
+logpiper.logpiper(port, (server) => {});
